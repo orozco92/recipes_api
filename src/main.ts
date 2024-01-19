621d2fbb1c98763ac0c54c5384962f1637dd405e
orozco92 <goldstein.david92@gmail.com>
2024-01-19 12:01:51 -0500

    * Fix linter * Remove specs * Adding swagger * Setting up swagger * Adding user  entity and user module with crud opperations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Recipes API')
+    .setDescription(
+      'API for queriying cooking recipes. Colaborators can create new recipes. Authenticated users will be able to rate any recipes.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('users', 'user module crud opperations')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
